a153510c455005e5313786941bb213880ff0dfe0
Robert-John van Doesburg <rjvandoesburg@gmail.com>
2018-02-25 14:19:54 +0100

    Finish step 1 of FizzBuzz test

---
+++ README.md
@@ -0,0 +1,3 @@
+# FizzBuzz example
+
+This repository is an example on how to solve the FizzBuzz questions sometimes asked during interviews
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Simple FizzBuzz example\n\n";
+
+// "Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”."
+
+for ($i = 1; $i <= 100; $i++) {
+    echo "{$i}\n";
+}
