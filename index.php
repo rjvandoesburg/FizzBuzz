10c0635e11cf04de05f1d4cc0c61d3425401f05f
Robert-John van Doesburg <rjvandoesburg@gmail.com>
2018-02-25 14:21:29 +0100

    Finish step 2 op FizzBuzz

--- index.php
+++ index.php
@@ -7,5 +7,11 @@ echo "Simple FizzBuzz example\n\n";
 // For numbers which are multiples of both three and five print “FizzBuzz”."
 
 for ($i = 1; $i <= 100; $i++) {
-    echo "{$i}\n";
+    if ($i % 3 === 0) {
+        echo "Fizz\n";
+    } else if ($i % 5 === 0) {
+        echo "Buzz\n";
+    } else {
+        echo "{$i}\n";
+    }
 }
